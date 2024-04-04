cf8a8175a6368ee41dd61c81db8cfc29b0384ec3
teammates77 <priyankaroyal2507@gmail.com>
2024-04-04 09:40:53 +0530

    modifictions in restaurant service

--- Backend/RestaurantService/src/main/java/com/foodapp/restaurantservice/exceptions/GlobalExceptionHandler.java
+++ Backend/RestaurantService/src/main/java/com/foodapp/restaurantservice/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.foodapp.restaurantservice.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
@@ -67,6 +69,42 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
 
     }
+
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetails> methodArgumentExceptionHandler(MethodArgumentNotValidException me, WebRequest req)  {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setTimeSpan(LocalDateTime.now());
+        errorDetails.setDescription(req.getDescription(false));
+
+        // Check if getFieldError() returns null
+        FieldError fieldError = me.getBindingResult().getFieldError();
+        if (fieldError != null) {
+            errorDetails.setMessage(fieldError.getDefaultMessage());
+        } else {
+            // Handle the case where no field error is present
+            errorDetails.setMessage("Validation failed");
+        }
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    
+    
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorDetails> noHandlerExceptionHandler(NoHandlerFoundException exc, WebRequest req)  {
+
+        ErrorDetails errorDetails = new ErrorDetails();
+
+        errorDetails.setTimeSpan(LocalDateTime.now());
+        errorDetails.setMessage(exc.getMessage());
+        errorDetails.setDescription(req.getDescription(false));
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+
+    }
+    
+
 
 
 }
