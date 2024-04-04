cd3593d1052b5c66ca844d57d45c84b6c2759688
teammates77 <priyankaroyal2507@gmail.com>
2024-04-04 10:02:49 +0530

    apigateway

--- Backend/FoodApiGateway/src/main/java/com/foodapp/foodapigateway/FoodApiGatewayApplication.java
+++ Backend/FoodApiGateway/src/main/java/com/foodapp/foodapigateway/FoodApiGatewayApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
-
 public class FoodApiGatewayApplication {
 
 	public static void main(String[] args) {
